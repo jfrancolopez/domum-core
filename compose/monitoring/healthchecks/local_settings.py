2433d5a52bb6aa8f04fc007785fa68db5351f462
Franco <jfranco8@outlook.com>
2026-08-04 15:00:30 -0400

    healthchecks: allow lan csrf origins

--- compose/monitoring/healthchecks/local_settings.py
+++ compose/monitoring/healthchecks/local_settings.py
@@ -1,5 +1,17 @@
 import os
+from urllib.parse import urlparse
 
-CSRF_TRUSTED_ORIGINS = [os.environ["SITE_ROOT"]]
+site_root = os.environ["SITE_ROOT"].rstrip("/")
+site_host = urlparse(site_root).hostname
+base_domain = ".".join(site_host.split(".")[-2:]) if site_host else ""
+
+CSRF_TRUSTED_ORIGINS = [
+    site_root,
+    f"https://{site_host}",
+    f"http://{site_host}",
+]
+if base_domain:
+    CSRF_TRUSTED_ORIGINS.append(f"https://*.{base_domain}")
+
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
